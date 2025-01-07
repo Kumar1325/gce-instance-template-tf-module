9d6ed21c7c2624d936fdbc613bfadb51a3a40bbb
Kumar1325 <Kumar1325@users.noreply.github.com>
2025-01-07 10:15:10 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,30 +23,6 @@ variable "description" {
   default     = null
 }
 
-variable "enable_confidential_vm" {
-  description = "Enable confidential VM"
-  type        = bool
-  default     = false
-}
-
-variable "shielded_secure_boot" {
-  description = "Enable secure boot for Shielded VM"
-  type        = bool
-  default     = false
-}
-
-variable "shielded_vtpm" {
-  description = "Enable vTPM for Shielded VM"
-  type        = bool
-  default     = false
-}
-
-variable "shielded_integrity_monitoring" {
-  description = "Enable integrity monitoring for Shielded VM"
-  type        = bool
-  default     = false
-}
-
 variable "on_host_maintenance" {
   description = "Maintenance behavior"
   type        = string
@@ -89,30 +65,43 @@ variable "sole_tenancy_values" {
   default     = []
 }
 
+#######
+# disk
+#######
 variable "source_image" {
-  description = "Source image for the VM"
+  description = "Source disk image. If neither source_image nor source_image_family is specified, defaults to the latest public RHEL 8 optimized for GCP image."
   type        = string
+  default     = ""
 }
 
-variable "disk_size" {
-  description = "Disk size for the VM"
-  type        = number
-  default     = 10
+variable "source_image_family" {
+  description = "Source image family. If neither source_image nor source_image_family is specified, defaults to the latest public RHEL 8 optimized for GCP image."
+  type        = string
+  default     = "rhel-8"
+}
+
+variable "source_image_project" {
+  description = "Project where the source image comes from. The default project contains RHEL8 Linux images."
+  type        = string
+  default     = "rhel-cloud"
+}
+
+variable "disk_size_gb" {
+  description = "Boot disk size in GB"
+  type        = string
+  default     = "20"
 }
 
 variable "disk_type" {
-  description = "Disk type for the VM (pd-standard, pd-balanced, pd-ssd, etc.)"
+  description = "Boot disk type, can be pd-ssd, local-ssd, pd-balanced, pd-standard etc,."
   type        = string
   default     = "pd-balanced"
 }
 
-variable "cmek_key" {
-  description = "KMS key for CMEK encryption"
-  type        = string
-  validation {
-    condition     = can(regex("projects/.+/locations/.+/keyRings/.+/cryptoKeys/.+", var.cmek_key))
-    error_message = "Provide a valid CMEK key in the format projects/{project}/locations/{location}/keyRings/{keyRing}/cryptoKeys/{key}."
-  }
+variable "disk_labels" {
+  description = "Labels to be assigned to boot disk, provided as a map"
+  type        = map(string)
+  default     = {}
 }
 
 variable "additional_disks" {
@@ -124,4 +113,71 @@ variable "additional_disks" {
     disk_type = optional(string)
   }))
   default = []
+}
+
+###########
+# metadata
+###########
+
+variable "startup_script" {
+  description = "User startup script to run when instances spin up"
+  type        = string
+  default     = ""
+}
+
+variable "metadata" {
+  type        = map(string)
+  description = "Metadata, provided as a map"
+  default     = {}
+}
+
+##################
+# service_account
+##################
+
+variable "service_account" {
+  type = object({
+    email  = string
+    scopes = optional(set(string), ["cloud-platform"])
+  })
+  description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template#service_account."
+}
+
+###########################
+# Shielded VMs
+###########################
+variable "enable_shielded_vm" {
+  type        = bool
+  default     = false
+  description = "Whether to enable the Shielded VM configuration on the instance. Note that the instance image must support Shielded VMs. See https://cloud.google.com/compute/docs/images"
+}
+
+variable "shielded_instance_config" {
+  description = "Not used unless enable_shielded_vm is true. Shielded VM configuration for the instance."
+  type = object({
+    enable_secure_boot          = bool
+    enable_vtpm                 = bool
+    enable_integrity_monitoring = bool
+  })
+
+  default = {
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+    enable_integrity_monitoring = true
+  }
+}
+
+###########################
+# Confidential Compute VMs
+###########################
+variable "enable_confidential_vm" {
+  type        = bool
+  default     = false
+  description = "Whether to enable the Confidential VM configuration on the instance. Note that the instance image must support Confidential VMs. See https://cloud.google.com/compute/docs/images"
+}
+
+variable "confidential_instance_type" {
+  type        = string
+  default     = null
+  description = "Defines the confidential computing technology the instance uses. If this is set to \"SEV_SNP\", var.min_cpu_platform will be automatically set to \"AMD Milan\". See https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#confidential_instance_type."
 }
